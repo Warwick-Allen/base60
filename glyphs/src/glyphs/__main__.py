9b7908a6416481904e87c8b3a64803d5c1653e98
Warwick Allen, Datum Process <warwick@datumprocess.co.nz>
2026-05-23 22:14:13 +1200

    Add --stroke=64 to glyphs

--- glyphs/src/glyphs/__main__.py
+++ glyphs/src/glyphs/__main__.py
@@ -12,7 +12,6 @@ from glyphs.display import configure_matplotlib, show_or_save
 configure_matplotlib()
 import matplotlib.pyplot as plt  # noqa: E402
 
-
 def main() -> None:
     parser = argparse.ArgumentParser(description="Glyphs plotting toolkit")
     parser.add_argument(
@@ -45,20 +44,23 @@ def main() -> None:
         show_or_save(args.output)
     elif args.stroke is not None:
         k = 0.5
-        n = 500
+        n = 1000
         x = np.linspace(0, k, n)
         match args.stroke:
             case 60:
-                y1 = (1 - (4 * x - 1) ** 2) / 4
+                y1 = (1 - (4*x - 1)**2)/4
                 # log(2x) is undefined at x=0; limit of y2 is 0 there (y1=0, denom→∞)
-                y2 = np.zeros_like(y1)
                 positive = x > 0
+                y2 = np.zeros_like(x)
                 y2[positive] = y1[positive]/(1 - np.log(2*x[positive]))
+            case 64:
+                y1 = (1 - abs(4*x - 1)**(1/2))/2
+                y2 = y1*4/5
             case _:
                 raise ValueError(f"Unknown stroke: {args.stroke}")
         plt.fill_between(x, y1, y2, color='green')
-        plt.xlim(0, k)
-        plt.ylim(0, k)
+        plt.xlim(-k, k)
+        plt.ylim(-k, k)
         show_or_save(args.output)
     else:
         parser.print_help()
